a62189a0f0e31354aa6b3d277490b8929a594cc4
rhysd <lin90162@yahoo.co.jp>
2018-12-30 22:20:08 +0900

    add documentation

--- README.md
+++ README.md
@@ -3,11 +3,68 @@ Rust library to convert a file path from/to slash path
 [![CI on Linux and macOS][travis-ci-badge]][travis-ci]
 [![CI on Windows][appveyor-badge]][appveyor]
 
+[`path-slash`][crates-io] is a tiny library to convert a file path (e.g. `foo/bar`, `foo\bar` or
+`C:\\foo\bar`) from/to slash path (e.g. `foo/bar`, `C://foo/bar`).
+
+In Unix-like OS, path separator is slash `/` by default. So any conversion is not necessary. But on
+Windows, file path separator `\` needs to be replaced with slash `/` (and of course `\` for escaping
+character should not be replaced).
+
+This package was inspired by Go's [`path/filepath.FromSlash`](https://golang.org/pkg/path/filepath/#FromSlash)
+and [`path/filepath.ToSlash`](https://golang.org/pkg/path/filepath/#ToSlash).
+
+## Usage
+
+`path_slash::PathExt` and `path_slash::PathBufExt` traits are defined. By using them, `std::path::Path`
+and `std::path::PathBuf` gains some methods and associated functions
+
+- `PathExt`
+  - `Path::to_slash(&self) -> Option<String>`
+  - `Path::to_slash_lossy(&self) -> String`
+- `PathBufExt`
+  - `PathBuf::from_slash<S: AsRef<str>>(s: S) -> PathBuf`
+  - `PathBuf::from_slash_lossy<S: AsRef<OsStr>>(s: S) -> PathBuf`
+  - `PathBuf::to_slash(&self) -> Option<String>`
+  - `PathBuf::to_slash_lossy(&self) -> String`
+
+```rust
+fn example_path_ext() {
+    // Trait for extending std::path::Path
+    use path_slash::PathExt;
+
+    // On Windows
+    assert_eq!(Path::new(r"foo\bar\piyo.txt").to_slash(), Some("foo/bar/piyo.txt".to_string()));
+    assert_eq!(Path::new(r"C:\\foo\bar\piyo.txt").to_slash(), Some("C://foo/bar/piyo.txt".to_string()));
+}
+
+fn example_pathbuf_ext() {
+    // Trait for extending std::path::PathBuf
+    use path_slash::PathBufExt;
+
+    // On Windows
+    let p = PathBuf::from_slash("foo/bar/piyo.txt");
+    assert_eq!(p, PathBuf::from(r"foo\bar\piyo.txt"));
+    assert_eq!(p.to_slash(), Some("foo/bar/piyo.txt".to_string()));
+}
+```
+
+Please read [documents][doc] for more details.
+
+## Installation
+
+Add `path-slash` to dependencies:
+
+```toml
+[dependencies]
+path-slash = "0.x"
+```
 
 ## License
 
 [the MIT License](LICENSE.txt)
 
+[doc]: https://docs.rs/path-slash
+[crates-io]: https://crates.io/crates/path-slash
 [appveyor-badge]: https://ci.appveyor.com/api/projects/status/44t8q0viea89fm2e/branch/master?svg=true
 [appveyor]: https://ci.appveyor.com/project/rhysd/path-slash/branch/master
 [travis-ci-badge]: https://travis-ci.org/rhysd/path-slash.svg?branch=master
--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,107 @@
+//! A library for converting file paths from/to slash paths
+//!
+//! A slash path is a path whose components are separated by separator '/' always.
+//!
+//! In Unix-like OS, path separator is slash '/' by default. So any conversion is not necessary.
+//! But on Windows, file path separator '\' needs to be replaced with slash '/' (and of course '\'
+//! for escaping character should not be replaced).
+//!
+//! For example, a file path 'foo\bar\piyo.txt' can be converted to/from a slash path 'foo/bar/piyo.txt'
+//!
+//! This package was inspired by Go's `path/filepath.FromSlash` and `path/filepath.ToSlash`.
+//! - https://golang.org/pkg/path/filepath/#FromSlash
+//! - https://golang.org/pkg/path/filepath/#ToSlash
+//!
+//! ```rust
+//! use std::path::{Path, PathBuf};
+//!
+//! // Trait for extending std::path::Path
+//! use path_slash::PathExt;
+//! // Trait for extending std::path::PathBuf
+//! use path_slash::PathBufExt;
+//!
+//! #[cfg(target_os = "windows")]
+//! {
+//!     assert_eq!(Path::new(r"foo\bar\piyo.txt").to_slash(), Some("foo/bar/piyo.txt".to_string()));
+//!     assert_eq!(Path::new(r"C:\\foo\bar\piyo.txt").to_slash(), Some("C://foo/bar/piyo.txt".to_string()));
+//!
+//!     let p = PathBuf::from_slash("foo/bar/piyo.txt");
+//!     assert_eq!(p, PathBuf::from(r"foo\bar\piyo.txt"));
+//!     assert_eq!(p.to_slash(), Some("foo/bar/piyo.txt".to_string()));
+//! }
+//!
+//! #[cfg(not(target_os = "windows"))]
+//! {
+//!     assert_eq!(Path::new("foo/bar/piyo.txt").to_slash(), Some("foo/bar/piyo.txt".to_string()));
+//!     assert_eq!(Path::new("/foo/bar/piyo.txt").to_slash(), Some("/foo/bar/piyo.txt".to_string()));
+//!
+//!     let p = PathBuf::from_slash("foo/bar/piyo.txt");
+//!     assert_eq!(p, PathBuf::from(r"foo/bar/piyo.txt"));
+//!     assert_eq!(p.to_slash(), Some("foo/bar/piyo.txt".to_string()));
+//! }
+//! ```
 use std::ffi::OsStr;
 use std::path::{Path, PathBuf};
 
+/// Trait to extend std::path::Path.
+///
+/// ```
+/// use path_slash::PathExt;
+///
+/// assert_eq!(
+///     std::path::Path::new("foo").to_slash(),
+///     Some("foo".to_string()),
+/// );
+/// ```
 pub trait PathExt {
     fn to_slash(&self) -> Option<String>;
     fn to_slash_lossy(&self) -> String;
 }
 
 impl PathExt for Path {
+    /// Convert the file path into slash path as UTF-8 string.
+    ///
+    /// Any file path separators in the file path is replaced with '/'.
+    /// Any non-Unicode sequences are replaced with U+FFFD.
+    ///
+    /// On non-Windows OS, it is equivalent to `to_string_lossy().to_string()`
+    ///
+    /// ```
+    /// use std::path::Path;
+    /// use path_slash::PathExt;
+    ///
+    /// #[cfg(target_os = "windows")]
+    /// let s = Path::new(r"foo\bar\piyo.txt");
+    ///
+    /// #[cfg(not(target_os = "windows"))]
+    /// let s = Path::new("foo/bar/piyo.txt");
+    ///
+    /// assert_eq!(s.to_slash_lossy(), "foo/bar/piyo.txt".to_string());
+    /// ```
     #[cfg(not(target_os = "windows"))]
     fn to_slash_lossy(&self) -> String {
         self.to_string_lossy().to_string()
     }
 
+    /// Convert the file path into slash path as UTF-8 string.
+    ///
+    /// Any file path separators in the file path is replaced with '/'.
+    /// Any non-Unicode sequences are replaced with U+FFFD.
+    ///
+    /// On non-Windows OS, it is equivalent to `.to_string_lossy().to_string()`.
+    ///
+    /// ```
+    /// use std::path::Path;
+    /// use path_slash::PathExt;
+    ///
+    /// #[cfg(target_os = "windows")]
+    /// let s = Path::new(r"foo\bar\piyo.txt");
+    ///
+    /// #[cfg(not(target_os = "windows"))]
+    /// let s = Path::new("foo/bar/piyo.txt");
+    ///
+    /// assert_eq!(s.to_slash_lossy(), "foo/bar/piyo.txt".to_string());
+    /// ```
     #[cfg(target_os = "windows")]
     fn to_slash_lossy(&self) -> String {
         use std::path;
@@ -44,11 +134,49 @@ impl PathExt for Path {
         buf
     }
 
+    /// Convert the file path into slash path as UTF-8 string.
+    ///
+    /// Any file path separators in the file path is replaced with '/'.
+    /// When the path contains non-Unicode sequence, this method returns None.
+    ///
+    /// On non-Windows OS, it is equivalent to `.to_str().map(std::to_string())`
+    ///
+    /// ```
+    /// use std::path::Path;
+    /// use path_slash::PathExt;
+    ///
+    /// #[cfg(target_os = "windows")]
+    /// let s = Path::new(r"foo\bar\piyo.txt");
+    ///
+    /// #[cfg(not(target_os = "windows"))]
+    /// let s = Path::new("foo/bar/piyo.txt");
+    ///
+    /// assert_eq!(s.to_slash(), Some("foo/bar/piyo.txt".to_string()));
+    /// ```
     #[cfg(not(target_os = "windows"))]
     fn to_slash(&self) -> Option<String> {
         self.to_str().map(str::to_string)
     }
 
+    /// Convert the file path into slash path as UTF-8 string.
+    ///
+    /// Any file path separators in the file path is replaced with '/'.
+    /// When the path contains non-Unicode sequence, this method returns None.
+    ///
+    /// On non-Windows OS, it is equivalent to `.to_str().map(std::to_string())`
+    ///
+    /// ```
+    /// use std::path::Path;
+    /// use path_slash::PathExt;
+    ///
+    /// #[cfg(target_os = "windows")]
+    /// let s = Path::new(r"foo\bar\piyo.txt");
+    ///
+    /// #[cfg(not(target_os = "windows"))]
+    /// let s = Path::new("foo/bar/piyo.txt");
+    ///
+    /// assert_eq!(s.to_slash(), Some("foo/bar/piyo.txt".to_string()));
+    /// ```
     #[cfg(target_os = "windows")]
     fn to_slash(&self) -> Option<String> {
         use std::path;
@@ -74,6 +202,16 @@ impl PathExt for Path {
     }
 }
 
+/// Trait to extend std::path::PathBuf.
+///
+/// ```
+/// use path_slash::PathBufExt;
+///
+/// assert_eq!(
+///     std::path::PathBuf::from_slash("foo/bar/piyo.txt").to_slash(),
+///     Some("foo/bar/piyo.txt".to_string()),
+/// );
+/// ```
 pub trait PathBufExt {
     fn from_slash<S: AsRef<str>>(s: S) -> Self;
     fn from_slash_lossy<S: AsRef<OsStr>>(s: S) -> Self;
@@ -82,16 +220,51 @@ pub trait PathBufExt {
 }
 
 impl PathBufExt for PathBuf {
+    /// Convert the slash path (path separated with '/') to std::path::PathBuf.
+    ///
+    /// Any '/' in the slash path is replaced with the file path separator.
+    /// The replacements only happen on Windows since the file path separators on other OSes are the
+    /// same as '/'.
+    ///
+    /// On non-Windows OS, it is simply equivalent to std::path::PathBuf::from().
+    ///
+    /// ```
+    /// use std::path::PathBuf;
+    /// use path_slash::PathBufExt;
+    ///
+    /// let p = PathBuf::from_slash("foo/bar/piyo.txt");
+    ///
+    /// #[cfg(target_os = "windows")]
+    /// assert_eq!(p, PathBuf::from(r"foo\bar\piyo.txt"));
+    ///
+    /// #[cfg(not(target_os = "windows"))]
+    /// assert_eq!(p, PathBuf::from("foo/bar/piyo.txt"));
+    /// ```
     #[cfg(not(target_os = "windows"))]
     fn from_slash<S: AsRef<str>>(s: S) -> Self {
         PathBuf::from(s.as_ref())
     }
 
-    #[cfg(not(target_os = "windows"))]
-    fn from_slash_lossy<S: AsRef<OsStr>>(s: S) -> Self {
-        PathBuf::from(s.as_ref())
-    }
-
+    /// Convert the slash path (path separated with '/') to std::path::PathBuf.
+    ///
+    /// Any '/' in the slash path is replaced with the file path separator.
+    /// The replacements only happen on Windows since the file path separators on other OSes are the
+    /// same as '/'.
+    ///
+    /// On non-Windows OS, it is simply equivalent to std::path::PathBuf::from().
+    ///
+    /// ```
+    /// use std::path::PathBuf;
+    /// use path_slash::PathBufExt;
+    ///
+    /// let p = PathBuf::from_slash("foo/bar/piyo.txt");
+    ///
+    /// #[cfg(target_os = "windows")]
+    /// assert_eq!(p, PathBuf::from(r"foo\bar\piyo.txt"));
+    ///
+    /// #[cfg(not(target_os = "windows"))]
+    /// assert_eq!(p, PathBuf::from("foo/bar/piyo.txt"));
+    /// ```
     #[cfg(target_os = "windows")]
     fn from_slash<S: AsRef<str>>(s: S) -> Self {
         use std::path;
@@ -107,15 +280,104 @@ impl PathBufExt for PathBuf {
         PathBuf::from(s)
     }
 
+    /// Convert the slash path (path separated with '/') to std::path::PathBuf.
+    ///
+    /// Any '/' in the slash path is replaced with the file path separator.
+    /// The replacements only happen on Windows since the file path separators on other OSes are the
+    /// same as '/'.
+    ///
+    /// On Windows, any non-Unicode sequences are replaced with U+FFFD while the conversion.
+    /// On non-Windows OS, it is simply equivalent to std::path::PathBuf::from() and there is no
+    /// loss while conversion.
+    ///
+    /// ```
+    /// use std::ffi::OsStr;
+    /// use std::path::PathBuf;
+    /// use path_slash::PathBufExt;
+    ///
+    /// let s: &OsStr = "foo/bar/piyo.txt".as_ref();
+    /// let p = PathBuf::from_slash_lossy(s);
+    ///
+    /// #[cfg(target_os = "windows")]
+    /// assert_eq!(p, PathBuf::from(r"foo\bar\piyo.txt"));
+    ///
+    /// #[cfg(not(target_os = "windows"))]
+    /// assert_eq!(p, PathBuf::from("foo/bar/piyo.txt"));
+    /// ```
+    #[cfg(not(target_os = "windows"))]
+    fn from_slash_lossy<S: AsRef<OsStr>>(s: S) -> Self {
+        PathBuf::from(s.as_ref())
+    }
+
+    /// Convert the slash path (path separated with '/') to std::path::PathBuf.
+    ///
+    /// Any '/' in the slash path is replaced with the file path separator.
+    /// The replacements only happen on Windows since the file path separators on other OSes are the
+    /// same as '/'.
+    ///
+    /// On Windows, any non-Unicode sequences are replaced with U+FFFD while the conversion.
+    /// On non-Windows OS, it is simply equivalent to std::path::PathBuf::from() and there is no
+    /// loss while conversion.
+    ///
+    /// ```
+    /// use std::ffi::OsStr;
+    /// use std::path::PathBuf;
+    /// use path_slash::PathBufExt;
+    ///
+    /// let s: &OsStr = "foo/bar/piyo.txt".as_ref();
+    /// let p = PathBuf::from_slash_lossy(s);
+    ///
+    /// #[cfg(target_os = "windows")]
+    /// assert_eq!(p, PathBuf::from(r"foo\bar\piyo.txt"));
+    ///
+    /// #[cfg(not(target_os = "windows"))]
+    /// assert_eq!(p, PathBuf::from("foo/bar/piyo.txt"));
+    /// ```
     #[cfg(target_os = "windows")]
     fn from_slash_lossy<S: AsRef<OsStr>>(s: S) -> Self {
         Self::from_slash(s.as_ref().to_string_lossy().chars().as_str())
     }
 
+    /// Convert the file path into slash path as UTF-8 string.
+    ///
+    /// Any file path separators in the file path is replaced with '/'.
+    /// Any non-Unicode sequences are replaced with U+FFFD.
+    ///
+    /// On non-Windows OS, it is equivalent to `to_string_lossy().to_string()`
+    ///
+    /// ```
+    /// use path_slash::PathBufExt;
+    ///
+    /// #[cfg(target_os = "windows")]
+    /// let s = std::path::PathBuf::from(r"foo\bar\piyo.txt");
+    ///
+    /// #[cfg(not(target_os = "windows"))]
+    /// let s = std::path::PathBuf::from("foo/bar/piyo.txt");
+    ///
+    /// assert_eq!(s.to_slash_lossy(), "foo/bar/piyo.txt".to_string());
+    /// ```
     fn to_slash_lossy(&self) -> String {
         self.as_path().to_slash_lossy()
     }
 
+    /// Convert the file path into slash path as UTF-8 string.
+    ///
+    /// Any file path separators in the file path is replaced with '/'.
+    /// When the path contains non-Unicode sequence, this method returns None.
+    ///
+    /// On non-Windows OS, it is equivalent to `.to_str().map(std::to_string())`
+    ///
+    /// ```
+    /// use path_slash::PathBufExt;
+    ///
+    /// #[cfg(target_os = "windows")]
+    /// let s = std::path::PathBuf::from(r"foo\bar\piyo.txt");
+    ///
+    /// #[cfg(not(target_os = "windows"))]
+    /// let s = std::path::PathBuf::from("foo/bar/piyo.txt");
+    ///
+    /// assert_eq!(s.to_slash(), Some("foo/bar/piyo.txt".to_string()));
+    /// ```
     fn to_slash(&self) -> Option<String> {
         self.as_path().to_slash()
     }
